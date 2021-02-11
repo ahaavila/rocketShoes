edb488416fe0779e1ee7de10e6ac3f2ced05d755
Augusto Ávila <augusto.avila@usemobile.xyz>
2021-02-11 11:55:48 -0300

    Add history when some product is add to cart

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -6,10 +6,13 @@
 // takeEvery -> Se o usuário clicar no botão 2 vezes sem que a chamada a
 // api tenha finalizado, o takeEvey vai chamar a api 2 vezes.
 import { call, put, select, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '../../../services/api';
+import history from '../../../services/history';
 
 import { formatPrice } from '../../../util/format';
-import { addToCartSuccess, updateAmount } from './actions';
-import api from '../../../services/api';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   // Vai acessar a API
@@ -31,12 +34,12 @@ function* addToCart({ id }) {
 
   // Verifico se a quantidade selecionada é maior que a quantidade em estoque
   if (amount > stockAmount) {
-    console.tron.warn('ERRO');
+    toast.error('Quantidade solicitada fora de estoque');
     return;
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -47,7 +50,25 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
